069e7a2477c4882cdf5c35c4155bd58aed27469d
bobo3004 <404866461@qq.com>
2019-03-05 23:57:38 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -3,36 +3,27 @@
 #include<math.h>
 using namespace std;
 
-class hax
-{
-    int low=0;
-    int high=0;
-    int mid=0;
-    int guess=0;
-    public:
-    int testFunction(int array[],int num){
-        high=sizeof(array)/sizeof(array[0]);
-        while(low<=high){
-            mid=floor(low+high)/2;
-            guess=array[mid];
-            if(guess==num){
-                return mid;
-            }
-            else if(guess>num)
-            {
-                high--;
-            }
-            else if(guess<num)
-            {
-                low++;
-            }
-            return 0;   
-        }
-    }
-};
+
 int main(){
-    int testArray[]={10,223,244,6666,432};
-    hax test1;
-    cout<<"finding number 6666"<<test1.testFunction(testArray,6666);
-    return;
+    vector<int> obj;
+    for(int i = 0; i < 10; i++)
+    {
+        obj.push_back(i);
+    }
+    
+    vector<int>::iterator it;
+    for(it=obj.begin();it!=obj.end();it++){
+        cout<<*it<<endl;
+    }
+    for(int i = 0; i < 10; i++)
+    {
+        obj.pop_back();
+    }
+    
+    for(it=obj.begin();it!=obj.end();it++){
+        cout<<*it<<endl;
+        cout<<"no nums";
+    }
+    
+
 }
