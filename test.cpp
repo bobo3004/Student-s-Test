7193fd0a305ffca217e17baf515deb3421cdb203
bobo3004 <404866461@qq.com>
2019-03-11 08:14:38 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,38 +1,10 @@
-#include<iostream>
-#include<vector>
-
-using namespace std;
-
-class hax
-{
-    int low=0;
-    int high=0;
-    int mid=0;
-    int guess=0;
-    public:
-    int testFunction(int array[],int num){
-        high=sizeof(array)/sizeof(array[0]);
-        while(low<=high){
-            mid=(low+high)/2;
-            guess=array[mid];
-            if(guess==num){
-                return mid;
-            }
-            else if(guess>num)
-            {
-                high--;
-            }
-            else if(guess<num)
-            {
-                low++;
-            }
-            return 0;   
-        }
-    }
-};
-int main(){
-    int testArray[]={10,223,244,6666,432};
-    hax test1;
-    cout<<"finding number 6666"<<test1.testFunction(testArray,6666);
-    return;
+#include<bits/stdc++.h>  
+using namespace std;  
+  
+int main(){  
+    cout<<"hello"<<endl;  
+    int u;  
+    while(cin>>u){  
+        cout<<u*u<<endl;  
+    }  
 }
